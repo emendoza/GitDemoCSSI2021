32c857273f47380c3b3caafbf5d230e1ddad5214
Lighty <30331804+jdz521@users.noreply.github.com>
2021-07-29 16:42:23 -0400

    Adding Apple class in script.js

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ function setUP() {
 function draw() {
 	// draw
 }
+
+// Apple class for Snake game.
+// Author: Jason
+class Apple {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = green;
+  }
+
+  hasCollided() {
+    // Checks here.
+  }
+}
